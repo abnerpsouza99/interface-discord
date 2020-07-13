dd5248d65985ae2d79b09c31d884dbeca9d31276
Abner Pena de Souza <abner.souza@crm.gov.br>
2020-07-13 16:14:07 -0300

    First commit

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Grid} from './styles'
+// Maneira de exportar a função com TypeScript
+
+const Layout: React.FC = function(){
+    return (
+        <Grid>
+            
+        </Grid>
+    );
+}
+
+export default Layout;
---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from './styles'
+
+const ServerList: React.FC = function(){
+    return (
+        <>
+        </>
+    )
+
+}
+
+export default ServerList;
